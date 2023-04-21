94ecefe88b0ed0eeee46ccf3315ddbd3e6e650ad
KEENAN KIRK <kakirk@loyola.edu>
2023-04-20 21:23:56 -0400

    Account CRUD #1 More work on account view

--- website/src/Pages/AccountPage.js
+++ website/src/Pages/AccountPage.js
@@ -8,54 +8,44 @@ import { SectionHeading } from "Treact/components/misc/Headings.js";
 
 const Container = tw(ContainerBase)`min-h-screen bg-primary-900 text-white font-medium flex justify-center -m-8`;
 const Content = tw.div`max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg flex justify-center flex-1`;
-const MainContainer = tw.div`lg:w-1/2 xl:w-5/12 p-6 sm:p-12`;
-const MainContent = tw.div`mt-12 flex flex-col items-center`;
-const Heading = tw.h1`bg-blue-100 text-2xl xl:text-3xl font-extrabold text-center`;
-const SubHeading = tw.h1`text-2xl xl:text-2xl font-extrabold bg-blue-100`;
-const Info = tw(SectionHeading)`mt-2 text-left text-gray-700 leading-snug xl:text-xl`;
-
+const MainContainer = tw.div`lg:w-full xl:w-full p-6 sm:p-12`;
+const MainContent = tw.div`mt-16 grid grid-cols-4 gap-4 items-center bg-blue-100`;
+const Heading = tw.h1`col-span-3 bg-blue-100 text-2xl xl:text-3xl font-extrabold text-center`;
+const SubHeading = tw.h1`col-span-2 text-2xl xl:text-2xl font-extrabold rounded-lg border-2 border-indigo-500`;
+const Info = tw(SectionHeading)`mt-2 text-center text-gray-700 leading-snug xl:text-xl`;
 
 export default function AccountView(){
-    return (
-        <AnimationRevealPage>
-          <Container>
-            <Content>
-              <MainContainer>
-                <Heading>_</Heading>
-                <MainContent>
-                    <SubHeading>Contact Information</SubHeading>
+  return (
+      <AnimationRevealPage>
+        <Container>
+          <Content>
+            <MainContainer>
+              <Heading>Account Details</Heading>
+              <MainContent>
+                  <SubHeading>Contact Information
                     <Info>Email: (make textboxes)</Info>
                     <Info>Phone: (make textboxes)</Info>
                     <Info>Address: (make textboxes)</Info>
-                </MainContent>
-                
-                <MainContent>
-                    <SubHeading>Child Info</SubHeading>
+                  </SubHeading>
+                  <SubHeading>Child Info
                     <Info>Name: </Info>
                     <Info>Age: </Info>
-                    <Info>Services: </Info>
-                </MainContent>
-              </MainContainer>
-
-              <MainContainer>
-                <Heading>Account Details</Heading>
-                <MainContent>
-                    <SubHeading>Upcoming Appointments</SubHeading>
-                </MainContent>
-              </MainContainer>
-
-              <MainContainer>
-                <Heading>_</Heading>
-                <MainContent>
-                    <SubHeading>Recent Appointments</SubHeading>
-                </MainContent>
-              </MainContainer>
-
-            </Content>
-          </Container>
-          <Footer/>
-        </AnimationRevealPage>
-    );
-
+                    <Info>Grade: </Info>
+                  </SubHeading>
+              
+                  <SubHeading>Upcoming Appointments
+                    <Info>info here </Info>
+                  </SubHeading>
+              
+                  <SubHeading>Recent Appointments
+                    <Info>info here </Info>
+                  </SubHeading>
+              </MainContent>
+            </MainContainer>
+          </Content>
+        </Container>
+        <Footer/>
+      </AnimationRevealPage>
+  );
 }
 
