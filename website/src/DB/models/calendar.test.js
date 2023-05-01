7e326bc07fa08a2b1b8137b5d441e09eb3174a05
KEENAN KIRK <kakirk@loyola.edu>
2023-05-01 10:35:29 -0400

    tweaked db model and updated testing

--- website/src/DB/models/calendar.test.js
+++ website/src/DB/models/calendar.test.js
@@ -7,14 +7,14 @@ afterEach(async function(){
 });
 
 test("Create new calendar", async function(){
-    let newCal = {scheduleId: "123", mondayTimes: ['08:00','09:00','10:00'], tuesdayTimes: ['08:00','09:00','10:00'], wednesdayTimes: ['08:00','09:00','10:00'], thursdayTimes: ['08:00','09:00','10:00'], fridayTimes: ['08:00','09:00','10:00']};
+    let newCal = {customer: "test", childName: "test kid", date: "05/10", time: "11:00", service: "lesson", location: "virtual"};
     let created = await calendarDB.create(newCal);
     let found = await calendarDB.read(created._id);
     expect(created).toBe(found);
 });
 
 test("Delete calendar", async function(){
-    let newCal = {scheduleId: "123", mondayTimes: ['08:00','09:00','10:00'], tuesdayTimes: ['08:00','09:00','10:00'], wednesdayTimes: ['08:00','09:00','10:00'], thursdayTimes: ['08:00','09:00','10:00'], fridayTimes: ['08:00','09:00','10:00']};
+    let newCal = {customer: "test", childName: "test kid", date: "05/10", time: "11:00", service: "lesson", location: "virtual"};
     let created = await calendarDB.create(newCal);
     await calendarDB.del("123");
     let found = await calendarDB.read(created._id);
@@ -22,10 +22,10 @@ test("Delete calendar", async function(){
 });
 
 test("Update calendar", async function(){
-    let newCal = {scheduleId: "123", mondayTimes: ['08:00','09:00','10:00'], tuesdayTimes: ['08:00','09:00','10:00'], wednesdayTimes: ['08:00','09:00','10:00'], thursdayTimes: ['08:00','09:00','10:00'], fridayTimes: ['08:00','09:00','10:00']};
+    let newCal = {customer: "test", childName: "test kid", date: "05/10", time: "11:00", service: "lesson", location: "virtual"};
     let created = await calendarDB.create(newCal);
-    let updated = {scheduleId: "321", mondayTimes: ['08:00','09:00','10:00'], tuesdayTimes: ['08:00','09:00','10:00'], wednesdayTimes: ['08:00','09:00','10:00'], thursdayTimes: ['08:00','09:00','10:00'], fridayTimes: ['08:00','09:00','10:00']};
-    created = await calendarDB.update("123", updated);
+    let updated = {customer: "test", childName: "test kid 2", date: "05/10", time: "11:00", service: "lesson", location: "virtual"};
+    created = await calendarDB.update("test", updated);
     let found = await calendarDB.read(created._id);
     expect(created).toBe(found);
 });
